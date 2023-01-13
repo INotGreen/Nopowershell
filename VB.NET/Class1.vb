8bb9d57ff1614ffaa1d958714fc1244e762e6217
INotGreen <89376703+INotGreen@users.noreply.github.com>
2023-01-13 20:45:51 +0800

    Add files via upload

---
+++ VB.NET/Class1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Management.Automation
+
+Module Module1
+    Sub Main()
+        Dim ps As PowerShell = PowerShell.Create()
+        ps.AddScript("calc")
+        ps.AddCommand("Out-String")
+        Dim results = ps.Invoke()
+        For Each result In results
+            Console.WriteLine(result.ToString())
+        Next
+        Console.ReadLine()
+    End Sub
+End Module
