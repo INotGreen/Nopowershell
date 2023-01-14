fadb0958fb6a2af734bd01c5956d23f9158c9a25
INotGreen <89376703+INotGreen@users.noreply.github.com>
2023-01-14 23:51:39 +0800

    Add files via upload

---
+++ Csharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation.Runspaces;
+using System.IO;
+using System.Management.Automation;
+
+namespace Program
+{
+    public class Class1
+    {
+
+
+        private static string command = "calc";
+        public static StringBuilder StringBuilder = new StringBuilder();
+        public static void Main(string[] args)
+        {
+            //.WriteLine(script);
+            RunPS(command);
+        }
+        public static string RunPS(string commandS)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            Pipeline pipeline = rs.CreatePipeline();
+            pipeline.Commands.AddScript(commandS);
+
+            foreach (PSObject line in pipeline.Invoke())
+            {
+                StringBuilder.AppendLine(line.ToString());
+
+            }
+            rs.Dispose();
+            pipeline.Dispose();
+            rs.Close();
+            return StringBuilder.ToString();
+        }
+    }
+}
